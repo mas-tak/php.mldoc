4d3a617372ba1cfd488692fb1d5bec791701f62f
mas-tak <railarain+dev@gmail.com>
2020-07-21 20:21:04 +0900

    add seed

---
+++ database/seeds/DocumentsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DocumentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	    DB::table('documents')->truncate();
+	    DB::table('documents')->insert([
+	        'name'=>'Github Flavored Markdown',
+	        'path'=>dirname(__FILE__, 3) .'/storage/documents/gfm.md',
+	        'type'=>'md',
+	    ], [
+	        'name'=>'reStrycturedText',
+	        'path'=>dirname(__FILE__, 3) .'/storage/documents/rst.rst',
+	        'type'=>'rst',
+	    ]
+	    );
+    }
+}
